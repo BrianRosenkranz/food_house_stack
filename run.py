8b6fc0874a2aa68be15a1bc7135993f1b478da49
BrianRosenkranz <moracho_22@hotmail.com>
2023-02-01 15:28:23 +0000

    added: df for stack, consume, remain, budgt

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
+# import pandas to work with dataframe
 import pandas as pd
+# import gspreads to work with google sheets and API
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -11,15 +13,25 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file("creds.json")
 SCOPE_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPE_CREDS)
+# get the spread sheet
 SHEET = GSPREAD_CLIENT.open("house_food_stack")
-
+# get the worksheets in to variables
 stack = SHEET.worksheet("stack")
-
+consume = SHEET.worksheet("consume")
+remain = SHEET.worksheet("remain")
+budget = SHEET.worksheet("budget")
+# get the values from the worksheet variables.
 stack_list = stack.get_all_values()
+consume_list = consume.get_all_values()
+remain_list = remain.get_all_values()
+budget_list = budget.get_all_values()
 
+# the value worksheets in to dataframe with pandas
+df_stack = pd.DataFrame(stack_list)
+df_consume = pd.DataFrame(consume_list)
+df_remain = pd.DataFrame(remain_list)
+df_budget = pd.DataFrame(budget_list)
 
-df = pd.DataFrame (stack_list)
-
-print(df)
+print(df_budget)
 
 
